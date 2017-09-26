48add9a6aa50d828329c482a2e68e37f319573ac
Alex Lao <lao.alex97@gmail.com>
2017-09-26 12:00:28 -0600

    Processes all incoming messages. Does not reply to itself.

--- processMessage.php
+++ processMessage.php
@@ -1,16 +1,25 @@
 <?php
-
+	include 'library.php';
 	if($_SERVER['REQUEST_METHOD']==='POST'){
-		$message = $_POST['text'];
+		$post = json_decode(file_get_contents('php://input'),true);
+		if($post['name']==='Crypto Price'){
+			return;
+		}
+		$message = $post['text'];
+		/*echo 'Message is: ' . $message;
+		sendMessage($message);*/
 		$demand = explode(' ', $message);
+		echo $demand[0];
 		if($demand[0][0]==='!'){
+			sendMessage('Command is: ' . $demand[1]);
 			//it is some type of demand
-			
+			//sendMessage($message);
 		}
 		else{
 			//ignore, it is not a type of demand
 			return;
 		}
 	}
-
+	//sendMessage("hi");
+	echo 'hi';
 ?>
