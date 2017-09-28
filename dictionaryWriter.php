88f64e596c7c626374e07ebb43839db7697eb2f5
Alex Lao <lao.alex97@gmail.com>
2017-09-28 15:44:53 -0600

    Creates a table in a database with the currency name and its respective symbol. All information gathered from a response to CoinMarketCap's API. This script will run every x days to get the newest coins.

--- dictionaryWriter.php
+++ dictionaryWriter.php
@@ -5,10 +5,39 @@
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 	$data = curl_exec($ch);
-
 	$values = json_decode($data, true);
 	print_r($values);
 
+	$servername = "localhost";
+	$username = "alexaqj1_admin";
+	$pw = "Al3xL4oz!!";
+	$dbname = "alexaqj1_cryptos";
+	
+	$connectToDB=new mysqli($servername, $username, $pw, $dbname);
 
+	if($connectToDB->connect_error){
+		die("Connection to DB failed: " . $connectToDB->connect_error);
+	}
+	/*	
+	$table = "DROP TABLE IF EXISTS 'currencies';";
+	$table .= "CREATE TABLE currencies(currencyName VARCHAR(32) PRIMARY KEY, symbol VARCHAR(16));";
+	if($connectToDB->multi_query($table)===TRUE){
+		echo 'created successfully';
+	}	
+	else{
+		echo 'error ' . $connectToDB->error;
+	}*/
+	if(!$connectToDB->query("DROP TABLE IF EXISTS currencies")||!$connectToDB->query("CREATE TABLE currencies(currencyName VARCHAR(30),symbol VARCHAR(12))")){
+		echo 'Table creation failed: (' . $connectToDB->errno . ') '. $connectToDB->error . "\n";
+	}
+	foreach($values as $item){
+		$insertStatement = "INSERT INTO currencies(currencyName, symbol) VALUES ('" . $item['name'] . "','" . $item['symbol'] . "')"; 
+		if($connectToDB->query($insertStatement)===FALSE){
+			echo 'Query Error: ' . $connectToDB->error . "\n";
+		}
+	
+		echo 'in loop ' . $insertStatement . "\n";
+	}
+	$connectToDB->close();
 
 ?>
