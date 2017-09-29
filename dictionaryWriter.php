968fa9218f28c02f75b01f20c1a1f2bbc3d1e5b1
Alex Lao <lao.alex97@gmail.com>
2017-09-29 17:15:08 -0600

    Made dictionaryWriter enter 120 coins into the database. Throroughly documented this script.

--- dictionaryWriter.php
+++ dictionaryWriter.php
@@ -1,6 +1,9 @@
 <?php
+	//Script that is programmed to run weekly with cron. Will update dictionary that is used to validate currencies requested by users with the top coins.
+
+	//Creates and executes curl request to get info on the top 120 coins. 
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://api.coinmarketcap.com/v1/ticker/?limit=10");
+	curl_setopt($ch, CURLOPT_URL, "https://api.coinmarketcap.com/v1/ticker/?limit=120");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
@@ -8,9 +11,10 @@
 	$values = json_decode($data, true);
 	print_r($values);
 
+	//Initialize a connection to the MySQL server.
 	$servername = "localhost";
 	$username = "alexaqj1_admin";
-	$pw = "Al3xL4oz!!";
+	$pw = "";
 	$dbname = "alexaqj1_cryptos";
 	
 	$connectToDB=new mysqli($servername, $username, $pw, $dbname);
@@ -18,18 +22,14 @@
 	if($connectToDB->connect_error){
 		die("Connection to DB failed: " . $connectToDB->connect_error);
 	}
-	/*	
-	$table = "DROP TABLE IF EXISTS 'currencies';";
-	$table .= "CREATE TABLE currencies(currencyName VARCHAR(32) PRIMARY KEY, symbol VARCHAR(16));";
-	if($connectToDB->multi_query($table)===TRUE){
-		echo 'created successfully';
-	}	
-	else{
-		echo 'error ' . $connectToDB->error;
-	}*/
+	
+	//Everytime this script is run, drop the current table and simply create a new one.
+	//This is necessary because some of the past top 120 currencies may no longer be in such a position.
 	if(!$connectToDB->query("DROP TABLE IF EXISTS currencies")||!$connectToDB->query("CREATE TABLE currencies(currencyName VARCHAR(30),symbol VARCHAR(12))")){
 		echo 'Table creation failed: (' . $connectToDB->errno . ') '. $connectToDB->error . "\n";
 	}
+
+	//Add every item to the database. Prepared statements are unnecessary because symbol and currency name will not have such symbols.
 	foreach($values as $item){
 		$insertStatement = "INSERT INTO currencies(currencyName, symbol) VALUES ('" . $item['name'] . "','" . $item['symbol'] . "')"; 
 		if($connectToDB->query($insertStatement)===FALSE){
@@ -38,6 +38,7 @@
 	
 		echo 'in loop ' . $insertStatement . "\n";
 	}
+	//Terminate initalized connections.
 	$connectToDB->close();
-
+	curl_close($ch);
 ?>
